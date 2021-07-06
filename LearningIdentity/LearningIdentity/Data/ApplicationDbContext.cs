a9f23b701f7b1bf3cf73212b52266c516ce5456a
Mark Dibe <dibemark.job@gmail.com>
2021-07-06 21:15:14 +0300

    first commit
    asp net identity for mvc and webapi

---
+++ LearningIdentity/LearningIdentity/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LearningIdentity.ViewModels;
+using LearningIdentity.Models;
+
+namespace LearningIdentity.Data
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+        public virtual DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
